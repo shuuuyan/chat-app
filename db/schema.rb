ae158501efde5f8d8267168e02ce6834727ae7ae
shuuuyan <shuchan1023@gmail.com>
2021-02-09 19:11:58 +0900

    ユーザー編集機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_062220) do
+ActiveRecord::Schema.define(version: 2021_02_09_095457) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
